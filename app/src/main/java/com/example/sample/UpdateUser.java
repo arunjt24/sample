8f55b9465fc05d748fad62814c2149b6049e9d80
arunjt24@gmail.com <arunjt24@gmail.com>
2021-04-20 22:23:14 +0530

    update user change

---
+++ app/src/main/java/com/example/sample/UpdateUser.java
@@ -0,0 +1,35 @@
+package com.example.sample;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UpdateUser extends AppCompatActivity {
+
+    private Button username,password;
+    private LinearLayout f_username_update,f_password_update,update_user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_user);
+        username = findViewById(R.id.update_user_name);
+        password = findViewById(R.id.update_user_password);
+        f_password_update = findViewById(R.id.f_password_update);
+        f_username_update = findViewById(R.id.f_username_update);
+        update_user = findViewById(R.id.update_user);
+        username.setText("Update UserName");
+        password.setText("Update Password");
+        username.setOnClickListener(n->{
+            update_user.setVisibility(View.GONE);
+            f_username_update.setVisibility(View.VISIBLE);
+        });
+        password.setOnClickListener(n->{
+            update_user.setVisibility(View.GONE);
+            f_password_update.setVisibility(View.VISIBLE);
+        });
+    }
+}
